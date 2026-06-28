a96d2605b610121e984962c18b3ff0c0e0eb8836
Justin Moulton <justin.moulton.7@gmail.com>
2026-06-28 12:27:01 -0400

    feat: add required crypto functions

--- packages/core/src/crypto.test.ts
+++ packages/core/src/crypto.test.ts
@@ -1,6 +1,30 @@
 import { describe, expect, it } from "vitest";
 
-import { generateSecureRandomString } from "./crypto.js";
+import { generateHash, generateSecureRandomString, timingSafeEquals, uint8ArrayToBase64url } from "./crypto.js";
+
+describe("uint8ArrayToBase64url", () => {
+	const expectedAlphabet = /^[a-zA-Z0-9\_\-]*$/;
+
+	it("generates valid base64url strings", () => {
+		const randomInput = crypto.getRandomValues(new Uint8Array(1024));
+		const test = uint8ArrayToBase64url(randomInput);
+
+		expect(expectedAlphabet.test(test)).toBe(true);
+		expect(test.includes("+")).toBe(false);
+		expect(test.includes("/")).toBe(false);
+		expect(test.includes("=")).toBe(false);
+	});
+
+	it("generates valid base64url strings of very large length", () => {
+		const randomInput = crypto.getRandomValues(new Uint8Array(65536));
+		const test = uint8ArrayToBase64url(randomInput);
+
+		expect(expectedAlphabet.test(test)).toBe(true);
+		expect(test.includes("+")).toBe(false);
+		expect(test.includes("/")).toBe(false);
+		expect(test.includes("=")).toBe(false);
+	});
+});
 
 describe("generateRandomSecureString", () => {
 	it("generates random strings", () => {
@@ -12,16 +36,6 @@ describe("generateRandomSecureString", () => {
 		expect(test1).not.toEqual(test2);
 	});
 
-	it("generates base64url strings", () => {
-		const expectedAlphabet = /^[a-zA-Z0-9\_\-]*$/;
-		const test = generateSecureRandomString(512);
-
-		expect(expectedAlphabet.test(test)).toBe(true);
-		expect(test.includes("+")).toBe(false);
-		expect(test.includes("/")).toBe(false);
-		expect(test.includes("=")).toBe(false);
-	});
-
 	it("generates strings of appropriate lengths", () => {
 		const small = generateSecureRandomString(8);
 		const medium = generateSecureRandomString(64);
@@ -31,5 +45,48 @@ describe("generateRandomSecureString", () => {
 		expect(small.length).toBe(11);
 		expect(medium.length).toBe(86);
 		expect(large.length).toBe(683);
+	});
+});
+
+describe("generateHash", () => {
+	it("generates correct SHA-256 hashes", async () => {
+		const test1 = await generateHash("seshy", "SHA-256");
+		const test2 = await generateHash("ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYn", "SHA-256");
+
+		expect(test1).toEqual("bDTQNva7U0tDtll-ouBJpeQAumLZhNUhuEeutcXDVBc");
+		expect(test2).toEqual("mzYBmQHAcQDXjARJgJYw8FsKAkI9sUrBw7bsj-IVqhU");
+	});
+
+	it("generates correct SHA-384 hashes", async () => {
+		const test1 = await generateHash("seshy", "SHA-384");
+		const test2 = await generateHash("ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYn", "SHA-384");
+
+		expect(test1).toEqual("In6S8VD2arIyoO51v189b9Zg66GhsufLzjEa-RcB4njdCiy-27DeAkgalyY1PTVw");
+		expect(test2).toEqual("KvQUgCFULOJFONIM-Mu1nLvSRRDrlshXKRs6lvhrizN0QW39z8HSzreLsaWOfWmL");
+	});
+
+	it("generates correct SHA-512 hashes", async () => {
+		const test1 = await generateHash("seshy", "SHA-512");
+		const test2 = await generateHash("ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYn", "SHA-512");
+
+		expect(test1).toEqual("MfURG3pqShhSjMSl5_e2wvUclOTEiel0npSMmJXdpbNQDJ-2Cv7Ihayf2iTVM7qqfeuGTXBuUE2yECJXhSOv6w");
+		expect(test2).toEqual("LppSywmO48nMK8zwbobeHsYbMYMxoXOAGUj9v6u0P9TJNS9ml_9qVa3e0fG5EXHGXMaotaQGP3x9u4L2gZ5KVg");
+	});
+});
+
+describe("timingSafeEquals", () => {
+	it("succeeds for matching strings", () => {
+		expect(timingSafeEquals("seshy", "seshy")).toBe(true);
+		expect(timingSafeEquals("ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYn", "ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYn")).toBe(true);
+	});
+
+	it("fails for non-matching strings of same length", () => {
+		expect(timingSafeEquals("seshy", "yhses")).toBe(false);
+		expect(timingSafeEquals("ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYn", "ZTpv8Z9ppOmuO5sBibIX6gTL0orUFtYn")).toBe(false);
+	});
+
+	it("fails for strings of different lengths", () => {
+		expect(timingSafeEquals("seshy", "seshy2")).toBe(false);
+		expect(timingSafeEquals("ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYn", "ZTpv8Z9ppOmuO5sBibIX5gTL0orUFtYnn")).toBe(false);
 	});
 });
--- packages/core/src/crypto.ts
+++ packages/core/src/crypto.ts
@@ -1,13 +1,76 @@
+const CHUNK_SIZE = 0x2000; // Process base64url conversions in chunks of 8kb
+const textEncoder = new TextEncoder();
+
+/**
+ * Converts a Uint8Array to a base64url-encoded string.
+ * Removes padding ("=") characters.
+ * @param array to convert
+ * @returns base64-url encoded string
+ */
+export const uint8ArrayToBase64url = (array: Uint8Array): string => {
+	// First convert to binary string in chunks
+	let binary = "";
+	for (let i = 0; i < array.length; i += CHUNK_SIZE) {
+		const chunk = array.subarray(i, i + CHUNK_SIZE);
+		binary += String.fromCharCode(...chunk);
+	}
+
+	return btoa(binary).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+};
+
 /**
  * Generates a secure base64url-encoded string of the given size in bytes.
  * @param bytes number of bytes to generate
- * @returns base64url-encoded string
+ * @returns base64url-encoded random string
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
  */
 export const generateSecureRandomString = (bytes: number): string => {
 	const arr = crypto.getRandomValues(new Uint8Array(bytes));
-	return btoa(String.fromCharCode(...arr))
-		.replace(/\+/g, "-")
-		.replace(/\//g, "_")
-		.replace(/=+$/, "");
+	return uint8ArrayToBase64url(arr);
+};
+
+/**
+ * Approved hash algorithm for Seshy.
+ */
+export type SeshyHashAlgorithm = "SHA-256" | "SHA-384" | "SHA-512";
+
+/**
+ * Generates a base64url-encoded hash of the input string using the given algorithm.
+ * @param input to create a hash for
+ * @param algorithm Seshy-allowed algorithm to use
+ * @returns base64url-encoded hash of the input string
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+ */
+export const generateHash = async (input: string, algorithm: SeshyHashAlgorithm): Promise<string> => {
+	const hash = await crypto.subtle.digest(algorithm, textEncoder.encode(input));
+	const arr = new Uint8Array(hash);
+	return uint8ArrayToBase64url(arr);
+};
+
+/**
+ * Performs a time safe equals between the two provided strings.
+ * @param a string one to compare
+ * @param b string two to compare
+ * @returns true if they match; false otherwise
+ */
+export const timingSafeEquals = (a: string, b: string): boolean => {
+	const aArr = textEncoder.encode(a);
+	const bArr = textEncoder.encode(b);
+
+	const lengthsMatch = aArr.byteLength === bArr.byteLength;
+
+	// Perform comparison of a against a if lengths don't match to avoid early return
+	const right = lengthsMatch ? bArr : aArr;
+
+	// Perform byte-by-byte comparison against full input size
+	let diff = 0;
+	for (let i = 0; i < aArr.byteLength; i++) {
+		// XOR between two arrays returns 0 if they're equal, non-zero otherwise
+		diff |= aArr[i]! ^ right[i]!;
+	}
+
+	// Fold length mismatch into diff so that if lengths were different, diff > 0
+	diff |= aArr.byteLength ^ bArr.byteLength;
+
+	return diff === 0;
 };
